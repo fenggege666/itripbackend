07172617ffb4ef62949e3eef849bc35cc431bab9
吃鸡大魔王 <964823150@qq.com>
2019-04-08 16:41:07 +0800

    新增了add方法

--- itriputils/src/main/java/cn/itrip/common/ChineseToEnglish.java
+++ itriputils/src/main/java/cn/itrip/common/ChineseToEnglish.java
@@ -16,6 +16,7 @@ public class ChineseToEnglish {
         logger.info("word:"+word+" pingyin:"+pinyinArray[0].substring(0,pinyinArray[0].length()-1));
         return  pinyinArray[0].substring(0,pinyinArray[0].length()-1);
     }
+    public void add(){}
 
     public static void main(String args[]){
         String result=getPinYin("北京");
